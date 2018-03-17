e2ff95c327785fad650c8024fc8b4ea0bbe82d3c
weixirui <q576077015@gmail.com>
2018-03-16 18:22:51 -0700

    start

---
+++ src/components/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test">
+    This is my demo file.
+	{{msg}}
+  </div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				msg:"I am data message."
+			}			
+		}
+	}
+
+</script>
+
+
+<style scoped>
+.test{
+	color:red;
+}
+</style>
