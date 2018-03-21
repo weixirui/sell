b1814e0b151aad5b38adbce03968928466ac85d7
weixirui <q576077015@gmail.com>
2018-03-21 00:29:23 -0700

    Header updated

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,81 @@
 <template>
 	<div class="header">
-		{{poiInfo.name}}
+		<div class="top-wrapper">
+			<div class="back-wrapper">
+				<span class="icon-arrow_lift"></span>
+			</div>
+			<form class="search-wrapper">
+				<span class="search-icon"></span>
+				<input class="search-bar" type="text" placeholder="搜索店家内容" />
+			</form>
+			<div class="more-wrapper">
+				<a class="spelling-bt" href="#">拼单</a>
+				<div class="more-bt"> 
+					<i class="s-radius"></i>
+					<i class="s-radius"></i>
+					<i class="s-radius"></i>
+				</div>
+			</div>
+		</div>
+		<div class="content-wrapper">
+			<div class="icon" :style="head_bg">
+		
+			</div>
+			
+			<div class="name">
+				<h3>{{poiInfo.name}}</h3>
+			</div>
+			
+			<div class="collect">
+				<img src="../../../resource/img/star.png" />
+				<span>收藏</span>
+			</div>
+		</div>
+		
+		<div class="bulletin-wrapper">
+			<img class="icon" v-if="poiInfo.discounts2" :src="poiInfo.discounts2[0].icon_url"  />
+			
+			<span class="text" v-if="poiInfo.discounts2">{{poiInfo.discounts2[0].info}}</span>
+			
+			<div class="detail" v-if="poiInfo.discounts2">
+				{{poiInfo.discounts2.length}}个活动
+				<span class="icon-keyboard_arrow_right"></span>
+			</div>
+		</div>
+			
+		<div class="bg-wrapper" :style="content_bg">
+			
+		</div>
+		
+		<div class="bulletin-detail">
+			<div class="detail-wrapper">
+				<div class="content-wrapper">
+					<div class="icon" :style="head_bg"></div>
+					<h3 class="name">{{poiInfo.name}}</h3>
+					<p class="tip">
+						{{poiInfo.min_price_tip}}<i>|</i>
+						{{poiInfo.shipping_fee_tip}}<i>|</i>
+						{{poiInfo.delivery_time_tip}}<i>|</i>
+					</p>
+					
+					<p class="time">
+						配送时间：
+						{{poiInfo.shipping_time}}
+					</p>
+					<div class="discounts" v-if="poiInfo.discounts2">
+						<p>
+							<img :src="poiInfo.discounts2[0].icon_url"  />
+							<span>{{poiInfo.discounts2[0].info}}</span>
+						</p>
+					</div>
+				</div>
+				
+				<div class="close-wrapper">
+					<span class="icon-close"></span>
+				</div>
+			</div>
+			
+		</div>
 	</div>
 </template>
 
@@ -11,9 +86,19 @@
 				type:Object,
 				default:{}
 			}
+		},
+		computed:{
+			content_bg(){
+				return "background-image: url(" + this.poiInfo.head_pic_url +");"
+			},
+			head_bg(){
+				return "background-image: url("+this.poiInfo.pic_url+");" 
+			}
 		}
 	}
 </script>
 
 <style>
+	@import url("../../common/styles/icon.css");
+	@import url("Header.css");
 </style>
